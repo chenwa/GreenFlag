6abe120b84457cad9a4632b59595e9028c1ff8ca
Warren <warren@iwarren.com>
2020-01-25 17:33:23 -0500

    Added images

--- app/src/main/java/com/example/greenflag/MainActivity.java
+++ app/src/main/java/com/example/greenflag/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.greenflag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        this.setContentView(R.layout.activity_main);
+    }
+
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Hide the action bar.
+        getSupportActionBar().hide();
+    }
+    */
+
+    // Navigates us to the createAccount activity
+    public void createAccount(View view) {
+        Intent intent = new Intent(this, CreateAccount.class);
+        startActivity(intent);
     }
 }
